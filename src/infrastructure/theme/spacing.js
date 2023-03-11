b38c64e7f7b13eea490628ea81315aa5d7196e43
raulpuscas8 <raul.puscas86@gmail.com>
2023-03-11 22:22:54 +0200

    Bara de search

---
+++ src/infrastructure/theme/spacing.js
@@ -0,0 +1,6 @@
+export const lineHeights = {
+  title: "28px",
+  copy: "20px",
+};
+///////////////////////0//////1///////2//////3
+export const space = ["0px", "4px", "8px", "16px", "32px", "64px"];
