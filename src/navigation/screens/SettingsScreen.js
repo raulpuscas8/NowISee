f2684c7abe0849d8751ae5b335e4ec0f388869a2
raulpuscas8 <raul.puscas86@gmail.com>
2023-04-11 18:29:07 +0300

    Video call done

--- src/navigation/screens/SettingsScreen.js
+++ src/navigation/screens/SettingsScreen.js
@@ -1,15 +1,260 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Switch,
+} from "react-native";
+import FeatherIcon from "react-native-vector-icons/Feather";
 
-export default function SettingsScreen({ navigation }) {
+const SECTIONS = [
+  {
+    header: "Preferences",
+    items: [
+      { id: "language", icon: "globe", label: "Language", type: "select" },
+      { id: "eye", icon: "eye", label: "Color Blind", type: "toggle" },
+      { id: "wifi", icon: "phone-call", label: "Allow calls", type: "toggle" },
+    ],
+  },
+  {
+    header: "Help",
+    items: [
+      { id: "bug", icon: "flag", label: "Report Bug", type: "link" },
+      { id: "contact", icon: "mail", label: "Contact Us", type: "link" },
+    ],
+  },
+  {
+    header: "Saved contacts",
+    items: [
+      { id: "save", icon: "save", label: "Saved", type: "link" },
+      {
+        id: "download",
+        icon: "delete",
+        label: "Stergere Cont",
+        type: "link",
+      },
+    ],
+  },
+];
+
+export default function SettingsScreen() {
+  const [form, setForm] = useState({
+    language: "English",
+    eye: true,
+    wifi: false,
+  });
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text
-        onPress={() => navigation.navigate("Home")}
-        style={{ fontSize: 26, fontWeight: "bold" }}
-      >
-        Settings Screen
-      </Text>
-    </View>
+    <SafeAreaView style={{ backgroundColor: "#f6f6f6" }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Settings</Text>
+
+          <Text style={styles.subtitle}>You can change your settings here</Text>
+        </View>
+
+        <View style={styles.profile}>
+          <Image
+            source={{
+              uri: "https://scontent-otp1-1.cdninstagram.com/v/t51.2885-19/298523271_1721537208228401_1252704275417780049_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-otp1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=_dRhGlKYyxUAX-JuHkv&edm=ACWDqb8BAAAA&ccb=7-5&oh=00_AfAWthlNRt8Fm1XZtdytleWVUjBGHtdRCC_8DVRT1fPq2g&oe=64200FCB&_nc_sid=1527a3",
+            }}
+            style={styles.profileAvatar}
+          />
+
+          <Text style={styles.profileName}>Raul Pușcaș</Text>
+
+          <Text style={styles.profileEmail}>raul.puscas@icloud.com</Text>
+
+          <TouchableOpacity
+            onPress={() => {
+              // handle onPress
+            }}
+          >
+            <View style={styles.profileAction}>
+              <Text style={styles.profileActionText}>Edit Profile</Text>
+
+              <FeatherIcon color="#fff" name="edit" size={16} />
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        {SECTIONS.map(({ header, items }) => (
+          <View style={styles.section} key={header}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionHeaderText}>{header}</Text>
+            </View>
+            <View style={styles.sectionBody}>
+              {items.map(({ id, label, icon, type, value }, index) => {
+                return (
+                  <View
+                    key={id}
+                    style={[
+                      styles.rowWrapper,
+                      index === 0 && { borderTopWidth: 0 },
+                    ]}
+                  >
+                    <TouchableOpacity
+                      onPress={() => {
+                        // handle onPress
+                      }}
+                    >
+                      <View style={styles.row}>
+                        <FeatherIcon
+                          color="#616161"
+                          name={icon}
+                          style={styles.rowIcon}
+                          size={22}
+                        />
+
+                        <Text style={styles.rowLabel}>{label}</Text>
+
+                        <View style={styles.rowSpacer} />
+
+                        {type === "select" && (
+                          <Text style={styles.rowValue}>{form[id]}</Text>
+                        )}
+
+                        {type === "toggle" && (
+                          <Switch
+                            onChange={(val) => setForm({ ...form, [id]: val })}
+                            value={form[id]}
+                          />
+                        )}
+
+                        {(type === "select" || type === "link") && (
+                          <FeatherIcon
+                            color="#ababab"
+                            name="chevron-right"
+                            size={22}
+                          />
+                        )}
+                      </View>
+                    </TouchableOpacity>
+                  </View>
+                );
+              })}
+            </View>
+          </View>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 24,
+  },
+  section: {
+    paddingTop: 12,
+  },
+  sectionHeader: {
+    paddingHorizontal: 24,
+    paddingVertical: 8,
+  },
+  sectionHeaderText: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: "#a7a7a7",
+    textTransform: "uppercase",
+    letterSpacing: 1.2,
+  },
+  sectionBody: {
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: "#e3e3e3",
+  },
+  header: {
+    paddingLeft: 24,
+    paddingRight: 24,
+    marginBottom: 12,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "700",
+    color: "#1d1d1d",
+    marginBottom: 6,
+  },
+  subtitle: {
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#929292",
+  },
+  profile: {
+    padding: 16,
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: "#e3e3e3",
+  },
+  profileAvatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 9999,
+  },
+  profileName: {
+    marginTop: 12,
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#090909",
+  },
+  profileEmail: {
+    marginTop: 6,
+    fontSize: 16,
+    fontWeight: "400",
+    color: "#848484",
+  },
+  profileAction: {
+    marginTop: 12,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#007bff",
+    borderRadius: 12,
+  },
+  profileActionText: {
+    marginRight: 8,
+    fontSize: 15,
+    fontWeight: "600",
+    color: "#fff",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingRight: 24,
+    height: 50,
+  },
+  rowWrapper: {
+    paddingLeft: 24,
+    backgroundColor: "#fff",
+    borderTopWidth: 1,
+    borderColor: "#e3e3e3",
+  },
+  rowIcon: {
+    marginRight: 12,
+  },
+  rowLabel: {
+    fontSize: 17,
+    fontWeight: "500",
+    color: "#000",
+  },
+  rowValue: {
+    fontSize: 17,
+    color: "#616161",
+    marginRight: 4,
+  },
+  rowSpacer: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+});
